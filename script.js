5db9a2fd7d109db86b99e3e8307824947ed06cf5
josemar serafim <josemarserafim2016@gmail.com>
2021-08-29 17:50:40 +0100

    criar cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let canvas = document.getElementById("snacke");
+let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let bix = 32;
+let box = 32;
+let snacke= [];
+snacke[0]= {
+    x: 8 * box,
+    y:8*box
+}
 
 function criarBG(){
-    context.fillStyle= "black";
-    context.fillRext(0,0,16*BhxBrowser, 16*box);
+    context.fillStyle= "lightgreen";
+    context.fillRect(0, 0, 16*box, 16*box);
+}
+
+function criarCobra() {
+    for(i=0; i<snacke.length; i++){
+        context.fillStyle = "green"
+        context.fillRect(snacke[i].x, snacke[i].y, box, box);
+    }
 }
 
 criarBG();
+criarCobra();
