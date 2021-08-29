ea16b9756c1ae2d8f688be85c9ca93df4486726f
josemar serafim <josemarserafim2016@gmail.com>
2021-08-29 17:38:54 +0100

    criando background

---
+++ script.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById("snacke");
+let context = canvas.getContext("2d");
+let bix = 32;
+
+function criarBG(){
+    context.fillStyle= "black";
+    context.fillRext(0,0,16*BhxBrowser, 16*box);
+}
+
+criarBG();
